3441d022ec8909fb460e6b38c4b33c9fd9d18143
jorge <jorge@hotmail.com>
2021-02-15 11:18:31 +0100

    principal

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+            function suma_resta(){
+             /*suma de 0-100 y resta nuemros aleatorios */ 
+            }; 
+            
+            function multi_divi(){
+             /*tabla de multiplicar del 5 y tabla de divisiones entre 2 */ 
+            }; 
+               
+            
+        ?>
+    </body>
+</html>
