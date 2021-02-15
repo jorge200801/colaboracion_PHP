3c302c6f9b9d281b9b31ea5b5d3babc755b5b4fd
jorge <jorge@hotmail.com>
2021-02-15 11:37:41 +0100

    parte_jorge

--- index.php
+++ index.php
@@ -13,8 +13,19 @@
             
             function multi_divi(){
              /*tabla de multiplicar del 5 y tabla de divisiones entre 2 */ 
+                $num1=5;
+                for($num2=0;$num2<=10;$num2++){
+                  
+                    echo $num1.' x '.$num2.'= '.$num1*$num2.'<br>';
+                };
+                echo '<br>';
+                $div1=2;
+                for($div2=1;$div2<=10;$div2++){
+                    
+                    echo $div1.' / '.$div2.'= '.$div1/$div2.'<br>';
+                };
             }; 
-               
+            echo multi_divi();
             
         ?>
     </body>
