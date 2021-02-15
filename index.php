81cd5a88641852755ebba9085767ab025c698049
juanmi140999 <juanmiguel.saugar@campusfp.es>
2021-02-15 11:37:30 +0100

    Partejuanmi

--- index.php
+++ index.php
@@ -9,13 +9,30 @@
         <?php
             function suma_resta(){
              /*suma de 0-100 y resta nuemros aleatorios */ 
+                $suma = 0;
+                $resta1 = rand(0, 100);
+                $resta2 = rand(0, 100);
+                $resulresta = 0;
+                for($i=0;$i<=100;$i++){
+                    $suma = $suma + $i;
+                    
+                };
+                echo "La suma es: ",$suma;
+                echo "<hr>";
+                if($resta1>$resta2){
+                    $resulresta = $resta1 - $resta2;
+                    echo "La resta de ",$resta1,"-",$resta2,"= ",$resulresta;
+                }else{
+                    $resulresta = $resta2 - $resta1;
+                    echo "La resta de ",$resta2,"-",$resta1,"= ",$resulresta;
+                };
             }; 
             
             function multi_divi(){
              /*tabla de multiplicar del 5 y tabla de divisiones entre 2 */ 
             }; 
                
-            
+            echo suma_resta();
         ?>
     </body>
 </html>
